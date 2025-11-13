24a51659ce04e43a577fb381bd4487ce9c081e39
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-11-13 10:22:12 +0100

    ..

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
 import { loadProducts } from "./js/uiComponent.js";
 import { createButton } from "./js/buttons.js";
 
+let savedProducts = [];
+
 async function products() {
-    const cont  = document.getElementById('productList');
+    const cont = document.getElementById('productList');
+
     const allProducts = await loadProducts();
+
+    savedProducts = allProducts;
 
     allProducts.forEach(item => {
         cont.appendChild(item);
     })
 }
 
+
+function filterProducts(text) {
+    const cont = document.getElementById('productList');
+    cont.innerHTML = "";
+
+    const query = text.toLowerCase();
+
+    const filtered = savedProducts.filter(item => {
+        const title = item.querySelector('h3').textContent.toLowerCase();
+        return title.includes(query);
+    });
+
+    filtered.forEach(item => cont.appendChild(item));
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const { productsBtn, menBtn, womenBtn } = createButton();
+
+    const input = document.getElementById('searchInput');
+
 
     const btnContainer = document.getElementById('button-container');
     btnContainer.append(productsBtn, menBtn, womenBtn);
     productsBtn.addEventListener('click', () => {
-        products();
+        const value = input.value.trim();
+
+        if (value === "") {
+            products();
+        } else {
+            filterProducts(value);
+        }
     });
+    products();
 });
-
-const btn = document.getElementById("btn")
-
-btn.addEventListener('click', async () => {
-    console.log("THE BUTTON WAS PUSHED")
-
-  const q = document.getElementById("searchInput").value.toLowerCase();
-  const res = await fetch("https://fakestoreapi.com/products");
-  const data = await res.json();
-  const results = data.filter(p => p.title.toLowerCase().includes(q));
-  document.getElementById("productList").innerHTML = results
-  .map(
-    p => `
-      <div class="product">
-        <img src="${p.image}" alt="${p.title}" width="100">
-        <b>${p.title}</b><br>
-        $${p.price}
-      </div>me
-    `
-  )
-  .join("") || "Inga produkter hittades.";
-
-});
-
